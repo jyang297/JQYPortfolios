e238f27510ba71ecbba1d6f9a686b4d234339242
Chris Pennington <chris@codinginpublic.dev>
2025-02-04 17:00:30 -0700

    lesson-25-2025

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,13 @@
-import { file } from "astro/loaders";
+import { file, glob } from "astro/loaders";
 import { defineCollection, z } from "astro:content";
 
 export const collections = {
+  posts: defineCollection({
+    loader: glob({
+      pattern: "src/data/posts/**/*.md",
+      // generateId: ({ entry, data }) => data.title as unknown as string,
+    }),
+  }),
   team: defineCollection({
     loader: file("src/data/team.json"),
     schema: z.object({
