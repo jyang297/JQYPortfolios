60825f0f109e1fbd424aacb7e6f69a4b37faa5d4
Chris Pennington <chris@codinginpublic.dev>
2025-02-14 07:45:10 -0700

    module-5.2

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,37 +1,35 @@
 import { file, glob } from "astro/loaders";
 import { defineCollection, reference, z } from "astro:content";
-import { parse as parseToml } from "toml";
-import { parse as parseCsv } from "csv-parse/sync";
 
 export const collections = {
-	posts: defineCollection({
-		loader: glob({
-			pattern: "src/data/posts/**/*.md",
-		}),
-		schema: ({ image }) =>
-			z.object({
-				title: z.string().max(32),
-				tags: z.array(z.string()),
-				pubDate: z.coerce.date(),
-				isDraft: z.boolean(),
-				canonicalURL: z.string().url().optional(),
-				cover: image(),
-				coverAlt: z.string(),
-				author: reference("team"),
-				slug: z.string(),
-			}),
-	}),
-	team: defineCollection({
-		loader: file("src/data/team.json"),
-		schema: z.object({
-			name: z.string(),
-			role: z.string(),
-			email: z.string().email(),
-			department: z.enum([
-				"Engineering",
-				"Software Development",
-				"Product Design",
-			]),
-		}),
-	}),
+  posts: defineCollection({
+    loader: glob({
+      pattern: "src/data/posts/**/*.md",
+    }),
+    schema: ({ image }) =>
+      z.object({
+        title: z.string().max(32),
+        tags: z.array(z.string()),
+        pubDate: z.coerce.date(),
+        isDraft: z.boolean(),
+        canonicalURL: z.string().url().optional(),
+        cover: image(),
+        coverAlt: z.string(),
+        author: reference("team"),
+        slug: z.string(),
+      }),
+  }),
+  team: defineCollection({
+    loader: file("src/data/team.json"),
+    schema: z.object({
+      name: z.string(),
+      role: z.string(),
+      email: z.string().email(),
+      department: z.enum([
+        "Engineering",
+        "Software Development",
+        "Product Design",
+      ]),
+    }),
+  }),
 };
