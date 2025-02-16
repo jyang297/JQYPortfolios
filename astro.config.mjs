8fde8f766b77dcf5c6621b5ccf082bb52afc4a8c
Chris Pennington <chris@codinginpublic.dev>
2025-02-16 07:03:50 -0700

    module 5.8

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,11 @@ export default defineConfig({
   adapter: node({
     mode: "standalone",
   }),
+  // redirects: {
+  //   "/about": {
+  //     status: 302,
+  //     destination: "/",
+  //   },
+  // },
   // output: "server"
 });
